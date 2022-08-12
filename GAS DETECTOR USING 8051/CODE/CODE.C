233470bf88ea1b950b5eb4dea87ba8581e8c3209
Atul Dighe <109819207+DIGHEATUL@users.noreply.github.com>
2022-08-12 21:13:08 +0530

    Add files via upload

---
+++ GAS DETECTOR USING 8051/CODE/CODE.C
@@ -0,0 +1,78 @@
+
+
+#include<reg51.h>
+
+#define lcd P1
+ 
+sbit GAS=P2^3;
+
+ 
+sbit rs=P2^0; //register select
+sbit rw=P2^1; //RW
+sbit en=P2^2; //enable
+ 
+void lcd_init();
+void cmd(unsigned char);
+void dat(unsigned char);
+void delay();
+void lcd_string(char *s);
+ 
+void main()
+{
+    lcd_init();
+    lcd_string("LPG GAS DETECTOR");
+    while(1) {
+        if(GAS) {
+            cmd(0xc0);
+            lcd_string("               ");
+					
+            delay();
+        } else {
+            cmd(0xc0);
+            lcd_string("LPG DETECTED !!");
+		 delay();
+        }
+    }
+}
+ 
+void lcd_init()
+{
+    cmd(0x38);
+    cmd(0x0e);
+    cmd(0x06);
+    cmd(0x01);
+    cmd(0x80);
+}
+ 
+void cmd(unsigned char a)
+{
+    lcd=a;
+    rs=0;
+    rw=0;
+    en=1;
+    delay();
+    en=0;
+}
+ 
+void dat(unsigned char b)
+{
+    lcd=b;
+    rs=1;
+    rw=0;
+    en=1;
+    delay();
+    en=0;
+}
+ 
+void lcd_string(char *s)
+{
+    while(*s) {
+       dat(*s++);
+     }
+}
+ 
+void delay()
+{
+    unsigned int i;
+    for(i=0;i<20000;i++);
+}
